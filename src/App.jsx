aa4199abec3a8228ca67e49d456b966f5f3bacbb
crweingarten <whineyg@gmail.com>
2024-07-24 18:42:13 -0700

    email and icon

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
     <div className="copy"> although these sounds are intended to be utilitarian, we offer no preference on how you should experience them.</div>
     <div className="copy fuzzBlue"><a href="https://apps.apple.com/ca/app/homestate-a-texas-kitchen/id925093380">get fuzzzel</a></div>
     <div className="copy fuzzBlue"><a href="https://docs.google.com/document/d/17j7ji4mhJHSFeZpM137fevPFvDdav8qoHiwjelQSn2Q/edit?usp=sharing" target="_blank">press release</a></div>
-    <div className="copy fuzzBlue"><a href="mailto:fuzzzelnoise@gmail.com">contact</a></div>
+    <div className="copy fuzzBlue"><a href="mailto:christopher@fuzzzel.com">contact</a></div>
     <div className="copy fuzzBlue">
       <a href="https://x.com/fuzzzelnoise" target="_blank"><FontAwesomeIcon icon={faTwitter}/></a>
       <a href="https://www.instagram.com/fuzzzelnoise/" target="_blank"><FontAwesomeIcon icon={faInstagram}/></a>
