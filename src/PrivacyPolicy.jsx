93341f34498596dff83c32cae3778e02c6584348
crweingarten <whineyg@gmail.com>
2024-08-01 22:49:29 -0700

    privacy policy

---
+++ src/PrivacyPolicy.jsx
@@ -0,0 +1,20 @@
+
+
+function PrivacyPolicy() {
+
+  return (
+
+    <div className="bottom-container">    
+    <div className="copy">Welcome to Fuzzzel. Your privacy is important to us, and we are committed to protecting your personal information. This Privacy Policy outlines our practices regarding data collection and usage.</div>
+    <div className="copy"><b>1. No Personal Data Collection</b></div>
+    <div className="copy">Fuzzzel does not collect, store, or process any personal data. Personal data is defined as information that can be used to identify an individual, such as name, email address, phone number, or any other details that could be linked to an individual.</div>
+    <div className="copy"><b>2. Cookies and Tracking Technologies</b></div>
+    <div className="copy">Fuzzzel does not use cookies, tracking technologies, or any other methods to collect personal information about you. Any data we collect is non-personal and used solely for the purpose of improving the app.</div>
+    <div className="copy"><b>3. Changes to This Privacy Policy</b></div>
+    <div className="copy">We may update this Privacy Policy from time to time. Any changes will be posted on the app.</div>
+    </div>
+ 
+  )
+}
+
+export default PrivacyPolicy
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.jsx'
+import PrivacyPolicy from './PrivacyPolicy.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+
+  {path: "/privacy",
+    element: <PrivacyPolicy />}
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
